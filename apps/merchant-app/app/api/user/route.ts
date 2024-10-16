c9600697c9b462d642c43b36f6c799bcf1284ab1
SoumyajitGhosh <soumyajitghosh.official@gmail.com>
2024-10-16 16:50:37 +0530

    Testing check CI flow

--- apps/merchant-app/app/api/user/route.ts
+++ apps/merchant-app/app/api/user/route.ts
@@ -2,12 +2,20 @@ import { NextResponse } from "next/server";
 import prisma from "@repo/db/client";  // Default import, as prisma is exported as default
 
 export const GET = async () => {
-  await prisma.user.create({
-    data: {
-      email: "asd",
-          name: "adsads",
-          password: "123",
-          number: "1234"
+  await prisma.user.upsert({
+    where: {
+      email: "asd",  // Checking if user with this email exists
+    },
+    update: {
+      name: "adsads",  // If user exists, update these fields
+      password: "123",
+      number: "1234",
+    },
+    create: {
+      email: "asd",  // If user doesn't exist, create a new user
+      name: "adsads",
+      password: "123",
+      number: "1234",
     },
   });
   
